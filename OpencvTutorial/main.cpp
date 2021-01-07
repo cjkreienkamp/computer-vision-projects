393fa4eb529bf26c59a0a2c5d34d89f4071dcd3d
ckreienk <ckreienk@email.arizona.edu>
2021-01-07 16:38:01 -0600

    Initial Commit

---
+++ OpencvTutorial/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpencvTutorial
+//
+//  Created by Chris Kreienkamp on 1/7/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
